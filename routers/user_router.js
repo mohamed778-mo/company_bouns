31cc1fb6c4dca133d0a2b6d676e49b4794767a40
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-11-02 00:06:36 +0200

    Update user_router.js

--- routers/user_router.js
+++ routers/user_router.js
@@ -2,7 +2,20 @@ const express =require("express")
 const router  = express.Router()
 const {
     Register,Login,getUser,getAllUser,adminUser,unAdminUser
-    ,editData,deleteOneData,deleteAllData,loginOut,add_bouns,getMe,edit_bonus,delete_bonus,changePassword}=require("../controllers/user")
+    ,editData,deleteOneData,deleteAllData,
+    loginOut,getMe,changePassword,
+  add_bouns_echo,
+  edit_bonus_echo,
+  delete_bonus_echo,
+  add_bouns_absence,
+  edit_bonus_absence,
+  delete_bonus_absence,
+  add_bouns_readiness,
+  edit_bonus_readiness,
+  delete_bonus_readiness,
+  add_bouns_presence,
+  edit_bonus_presence,
+  delete_bonus_presence}=require("../controllers/user")
 const { auth,adminAuth }=require("../middleware/auth")
 
 
@@ -17,17 +30,24 @@ router.patch('/unadmin/:id',adminAuth,unAdminUser)
 router.delete('/delete/:id',adminAuth,deleteOneData)
 router.delete('/delete_all_student',adminAuth,deleteAllData)
 router.delete('/login_out',loginOut)
-router.post('/add_bonus/:id',adminAuth,add_bouns)
-router.put('/edit_bonus/:id/:award_id',adminAuth, edit_bonus);
-router.delete('/delete_bonus/:id/:award_id',adminAuth, delete_bonus);
 router.patch('/change_password',changePassword)
 
+router.post('/add_bonus_echo/:id',adminAuth,add_bouns_echo)
+router.put('/edit_bonus_echo/:id/:award_id',adminAuth, edit_bonus_echo);
+router.delete('/delete_bonus_echo/:id/:award_id',adminAuth, delete_bonus_echo);
+
+router.post('/add_bonus_absence/:id',adminAuth,add_bouns_absence)
+router.put('/edit_bonus_absence/:id/:award_id',adminAuth, edit_bonus_absence);
+router.delete('/delete_bonus_absence/:id/:award_id',adminAuth, delete_bonus_absence)
 
 
+router.post('/add_bonus_presence/:id',adminAuth,add_bouns_presence)
+router.put('/edit_bonus_presence/:id/:award_id',adminAuth, edit_bonus_presence);
+router.delete('/delete_bonus_presence/:id/:award_id',adminAuth, delete_bonus_presence)
 
-
-
-
+router.post('/add_bonus_readiness/:id',adminAuth,add_bouns_readiness)
+router.put('/edit_bonus_readiness/:id/:award_id',adminAuth, edit_bonus_readiness);
+router.delete('/delete_bonus_readiness/:id/:award_id',adminAuth, delete_bonus_readiness)
 
 module.exports = router;
 
